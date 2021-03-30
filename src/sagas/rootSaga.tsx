70f9c9c2d8bdb3266d31990a143e4c80b44b09cb
JohnVinova <JohnVinova>
2021-03-30 17:02:19 +0700

    refactor code: complete

--- src/sagas/rootSaga.tsx
+++ src/sagas/rootSaga.tsx
@@ -1,9 +1,6 @@
 import { fork, all, takeEvery } from "redux-saga/effects";
-// import { watchAuthSaga } from "./authSaga";
-
+import { watchAuthSaga } from "./authSaga";
 
 export default function* rootSaga() {
-  yield all([
-    // fork(watchAuthSaga),
-  ]);
+  yield all([fork(watchAuthSaga)]);
 }
